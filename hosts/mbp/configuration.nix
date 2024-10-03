62f4306a88cae3b7f05fe86020f2a1c342873225
Sebastian Fellner 💯 <fellnerseb@gmail.com>
2024-10-03 11:58:17 +0200

    automatically clean up

--- hosts/mbp/configuration.nix
+++ hosts/mbp/configuration.nix
@@ -1,4 +1,4 @@
-{ self, pkgs, ... }:
+{ self, pkgs, lib, ... }:
  {
       # The `system.stateVersion` option is not defined in your
       # nix-darwin configuration. The value is used to conditionalize
@@ -19,7 +19,12 @@
           mtr-gui
           asdf-vm # need to also load fish autocompletions in the fish init further down
           # pkgs.openmoji-color # font with openmoji emojis
+          gnumake
+          bun
+          ffmpeg
+          tesseract
           unstable.screen-pipe
+          
         ];
 
       # Auto upgrade nix package and the daemon service.
@@ -37,6 +42,34 @@
         interactiveShellInit = ''
           source "${pkgs.asdf-vm}/share/asdf-vm/asdf.fish"
           source "${pkgs.asdf-vm}/share/asdf-vm/completions/asdf.fish"
+        '';
+        # FIXME: This is needed to address bug where the $PATH is re-ordered by
+        # the `path_helper` tool, prioritising Apple’s tools over the ones we’ve
+        # installed with nix.
+        #
+        # This gist explains the issue in more detail: https://gist.github.com/Linerre/f11ad4a6a934dcf01ee8415c9457e7b2
+        # There is also an issue open for nix-darwin: https://github.com/LnL7/nix-darwin/issues/122
+        loginShellInit = let
+        # We should probably use `config.environment.profiles`, as described in
+        # https://github.com/LnL7/nix-darwin/issues/122#issuecomment-1659465635
+        # but this takes into account the new XDG paths used when the nix
+        # configuration has `use-xdg-base-directories` enabled. See:
+        # https://github.com/LnL7/nix-darwin/issues/947 for more information.
+        profiles = [
+          "/etc/profiles/per-user/$USER" # Home manager packages
+          "$HOME/.nix-profile"
+          "(set -q XDG_STATE_HOME; and echo $XDG_STATE_HOME; or echo $HOME/.local/state)/nix/profile"
+          "/run/current-system/sw"
+          "/nix/var/nix/profiles/default"
+        ];
+
+        makeBinSearchPath =
+          lib.concatMapStringsSep " " (path: "${path}/bin");
+        in
+        ''
+          # Fix path that was re-ordered by Apple's path_helper
+          fish_add_path --move --prepend --path ${makeBinSearchPath profiles}
+          set fish_user_paths $fish_user_paths
         '';
       };
       environment.shells = [ pkgs.bashInteractive pkgs.zsh pkgs.fish ];      
@@ -74,5 +107,11 @@
       users.users.sefe = {
         name = "sefe";
         home = "/Users/sefe";
+      };
+
+      nix.gc = {
+        automatic = true;
+        interval = { Weekday = 0; Hour = 0; Minute = 0; };
+        options = "--delete-older-than 30d";
       };
     }
