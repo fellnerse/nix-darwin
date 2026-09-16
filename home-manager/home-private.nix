5ae679f1af45a9d36047adaaaf780a3097886a47
Sebastian Fellner <hey@sebastianfellner.de>
2026-09-16 10:51:20 +0200

    feat(home-manager): add daily greedy brew cask autoupdate agent for private profile

--- home-manager/home-private.nix
+++ home-manager/home-private.nix
@@ -34,4 +34,25 @@
     '';
     target = "ghostty/config";
   };
+
+  # Homebrew automatic background cask updates (greedy: true)
+  # Runs daily at 04:00 (or immediately on wake if asleep)
+  launchd.agents.brew-autoupdate = {
+    enable = true;
+    config = {
+      ProgramArguments = [
+        "/bin/sh"
+        "-c"
+        "PATH=/opt/homebrew/bin:$PATH brew update && PATH=/opt/homebrew/bin:$PATH brew upgrade --cask --greedy"
+      ];
+      StartCalendarInterval = [
+        {
+          Hour = 4;
+          Minute = 0;
+        }
+      ];
+      StandardOutPath = "/Users/private/Library/Logs/brew-autoupdate.log";
+      StandardErrorPath = "/Users/private/Library/Logs/brew-autoupdate-error.log";
+    };
+  };
 }
